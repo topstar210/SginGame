37f8c12ba88d51825c63baed038c4d550993dc11
topstar210 <happycamper0210@gmail.com>
2023-12-29 19:20:19 +0300

    initial project

--- web3.js
+++ web3.js
@@ -1,4 +1,4 @@
-let web3, contract, userAddress;
+let web3, contract, userAddress, spinToken;
 
 // Define the images for the slot machine reels
 const reelImages = [
@@ -11,15 +11,19 @@ const reelImages = [
 	"./reel/7.png",
 ];
 // Variables to track the current state of the slot machine
+let startTime, reelElements;
 let isSpinning = false;
+const spinDuration = 3000; // Adjust the duration as needed
 
 let currentSpinResult = [0, 0, 0]; // Initialize with default values
 
 // Get the modal
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("load-dogespin-button");
-var confirmLoadBtn = document.getElementById("confirm-load");
-var closespan = document.getElementsByClassName("close")[0];
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("load-dogespin-button");
+const spinBtn = document.getElementById('spin-button');
+const cashOutBtn = document.getElementById('cash-out-button');
+const confirmLoadBtn = document.getElementById("confirm-load");
+const closespan = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
 	modal.style.display = "block";
@@ -35,49 +39,63 @@ window.onclick = function (event) {
 	}
 }
 
-confirmLoadBtn.onclick = function () {
-	var loadWCVal = document.querySelector('input[name="load-wc"]:checked').value;
+// handle click deposit button
+confirmLoadBtn.onclick = async function () {
+	disableButton(confirmLoadBtn);
+	const loadWCVal = document.querySelector('input[name="load-wc"]:checked').value;
+	const amount = await web3.utils.toWei(loadWCVal, "ether");
+
+	const chkRes = await checkAllowanceAndApprove(amount);
+	if (!chkRes) return;
+
 	// Call the contract's getGreeting function
-	contract.methods.depositWDOGE(loadWCVal).send({from: userAddress})
+	await contract.methods.depositWDOGE(amount).send({ from: userAddress })
 		.then(result => console.log(result))
 		.catch(error => console.error(error));
+
+	enableButton(confirmLoadBtn, 'Confirm');
 }
 
-function spinSlotMachine() {
-	if (isSpinning) return; // Prevent multiple spins
+// handle click "spin" button
+spinBtn.onclick = () => spinSlotMachine();
 
-	isSpinning = true;
-	// Simulate spinning by changing the images rapidly
-	const reelElements = document.querySelectorAll(".reel img");
-	const spinDuration = 3000; // Adjust the duration as needed
+// handle click cash out
+cashOutBtn.onclick = () => cashOut();
 
-	// Generate random results (0 to reelImages.length - 1) for each reel
-	const randomResults = [];
-	for (let i = 0; i < 3; i++) {
-		randomResults.push(Math.floor(Math.random() * reelImages.length));
-	}
-	// Perform the spinning animation
-	const startTime = Date.now();
-	function spin() {
-		const currentTime = Date.now();
-		const elapsedTime = currentTime - startTime;
-		if (elapsedTime >= spinDuration) {
-			// Stop spinning and display the result
-			isSpinning = false;
-			currentSpinResult = randomResults;
-			updateSlotMachine();
-			return;
+
+// Check allowance and approve the token
+async function checkAllowanceAndApprove(amount) {
+	const allowance = await spinToken.methods.allowance(userAddress, contractAddress).call();
+
+	if (allowance < amount) {
+		try {
+			const maxApprovalValue = "115792089237316195423570985008687907853269984665640564039457584007913129639935"; // Maximum uint256 value
+			const approveTx = await spinToken.methods.approve(contractAddress, maxApprovalValue).send({ from: userAddress });
+			console.log("Token approved for deposit", approveTx);
+		} catch (error) {
+			console.error("Error:", error);
+			return false
 		}
-		// Rotate the images
-		for (let i = 0; i < reelElements.length; i++) {
-			const newIndex = (randomResults[i] + Math.floor(elapsedTime / 100)) % reelImages.length;
-			reelElements[i].src = reelImages[newIndex];
-		}
-		requestAnimationFrame(spin);
+	} else {
+		console.log("Sufficient allowance already granted");
 	}
-	spin();
-	console.log('randomResults ==> ', randomResults);
+	return true;
 }
+
+// button disable
+function disableButton(btn) {
+	btn.innerHTML = 'Loading...';
+	btn.disabled = true
+}
+
+// enable button
+function enableButton(btn, btnTxt = '') {
+	btn.innerHTML = btnTxt;
+	btn.disabled = false;
+	modal.style.display = "none";
+	updateBalance();
+}
+
 
 // Function to update the slot machine display based on the current result
 function updateSlotMachine() {
@@ -87,9 +105,73 @@ function updateSlotMachine() {
 	}
 }
 
+// Perform the spinning animation
+async function spin(randomResults) {
+	const currentTime = Date.now();
+	const elapsedTime = currentTime - startTime;
+	if (elapsedTime >= spinDuration) {
+		// Stop spinning and display the result
+		isSpinning = false;
+		currentSpinResult = randomResults;
+		updateSlotMachine();
+		return;
+	}
+	// Rotate the images
+	for (let i = 0; i < reelElements.length; i++) {
+		const newIndex = (randomResults[i] + Math.floor(elapsedTime / 100)) % reelImages.length;
+		reelElements[i].src = reelImages[newIndex];
+	}
+	requestAnimationFrame(()=>spin(randomResults));
+}
+
+async function spinSlotMachine() {
+	if (isSpinning) return; // Prevent multiple spins
+	disableButton(spinBtn);
+
+	isSpinning = true;
+	reelElements = document.querySelectorAll(".reel img"); // Simulate spinning by changing the images rapidly
+
+	// Generate random results (0 to reelImages.length - 1) for each reel
+	// const randomResults = [];
+	// for (let i = 0; i < 3; i++) {
+	// 	randomResults.push(Math.floor(Math.random() * reelImages.length));
+	// }
+	
+	try {
+		const estimatedGas = await contract.methods.spin().estimateGas({from: userAddress});
+		const spinResTx = await contract.methods.spin().send({ from: userAddress, gas: estimatedGas + BigInt(6000) });
+		const spinRes = spinResTx.events.Spun.returnValues.result;
+		const rewardAmount = await web3.utils.fromWei(spinResTx.events.Spun.returnValues.rewardAmount, "ether");
+		console.log('rewardAmount === ', rewardAmount);
+
+		const randomResults = spinRes.map(val => parseInt(val));
+		startTime = Date.now();
+		await spin(randomResults);
+	} catch (error) {
+		console.error("Spin Err: ", error);
+	}
+	enableButton(spinBtn, 'Spin');
+}
+
+// cash out function 
+async function cashOut() {
+
+}
+
+async function updateBalance() {
+	const balance = await contract.methods.wDOGEBalances(userAddress).call();
+	const dcBalance = await contract.methods.DCBalances(userAddress).call();
+	const spinTkBalance = await web3.utils.fromWei(balance, "ether");
+	const rewardTkBalance = await web3.utils.fromWei(dcBalance, "ether");
+	document.getElementById('spin-balance').innerHTML = Number(spinTkBalance).toFixed(2);
+	document.getElementById('reward-balance').innerHTML = Number(rewardTkBalance).toFixed(2);
+}
+
 async function contractFunc() {
 	web3 = new Web3(window.ethereum);
 	contract = new web3.eth.Contract(abi, contractAddress);
+	spinToken = new web3.eth.Contract(spinTokenABI, spinTokenAddress);
+	rewardToken = new web3.eth.Contract(rewardTokenABI, rewardTokenAddress);
 
 	// Get the user's Ethereum address
 	const accounts = await web3.eth.getAccounts();
@@ -100,17 +182,7 @@ async function contractFunc() {
 	document.getElementById("addresss-div").style.display = 'block';
 	document.getElementById("connected-address").innerHTML = shortenedAddress;
 
-	const balance = await web3.eth.getBalance(userAddress);
-	const contractBalance = await web3.eth.getBalance(contractAddress);
-	const wDogeBalance = await web3.utils.fromWei(balance, "ether");
-	const DCTokenBalance = await web3.utils.fromWei(contractBalance, "ether");
-	document.getElementById('wDOGE-balance').innerHTML = parseInt(wDogeBalance);
-	document.getElementById('DC-token-balance').innerHTML = parseInt(DCTokenBalance);
-
-
-	// Enable the spin button
-	document.getElementById('spin-button').removeAttribute('disabled');
-	document.getElementById('spin-button').addEventListener('click', () => spinSlotMachine());
+	updateBalance();
 }
 
 // Function to initialize the app after connecting to MetaMask
@@ -125,8 +197,8 @@ async function initializeApp() {
 		contractFunc();
 	} else {
 		alert('Web3 is not initialized. Please connect to MetaMask first.');
-		document.getElementById('DC-token-balance').innerText = "0"
-		document.getElementById('wDOGE-balance').innerText = "0"
+		document.getElementById('reward-balance').innerText = "0"
+		document.getElementById('spin-balance').innerText = "0"
 		return;
 	}
 
